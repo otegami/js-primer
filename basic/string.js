f5b2f83c663bd121008d1ed464fc0201aa3e882b
otegami <a.s.takuya1026@gmail.com>
2020-12-23 11:38:08 +0900

    fix my understanding about string

---
+++ basic/string.js
@@ -0,0 +1,18 @@
+// const str = "にわにはにわにわとりがいる";
+
+// console.log(str.indexOf("にわ"));
+// console.log(str.lastIndexOf("にわ"));
+// console.log(str.indexOf("未知のキーワード"));
+
+// const str = "JavaScript";
+// const searchWord = "Script";
+// const index = str.indexOf(searchWord);
+
+// if (index !== -1) {
+//   console.log(`${searchWord}が見つかりました`);
+// } else {
+//   console.log(`${searchWord}は見つかりませんでした`);
+// }
+
+const str = "にわにはにわにわとりがいる";
+console.log(str.startsWith("にわ"));
